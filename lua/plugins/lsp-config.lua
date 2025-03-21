e7571e0694528a2b52ed646b0712144ca8a69446
rroig <rroig7@gmail.com>
2025-03-21 12:08:14 -0400

    got rid of ast_grep cause it was causing issues

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,8 +11,7 @@ return {
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
-                    "clangd",
-                    "ast_grep"
+                    "clangd" 
                 }
             })
         end
