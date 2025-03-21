a228ac87b3c0e59ad453ab7aac18dd841473191e
Sinh <blackwolf7901@gmail.com>
2025-03-20 23:43:32 -0400

    adding a few plugins, such as lspconfig and others

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,7 @@ return
     name = "catppuccin",
     priority = 1000,
     config = function()
+        --vim.cmd.colorscheme "catppuccin-macchiato"
         vim.cmd.colorscheme "vscode"
     end
 }
